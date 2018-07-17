dcf2baa917fd44f3a5eb854e612042280922168a
C90Gray <cmusic.gray@gmail.com>
2018-07-17 00:34:12 -0400

    fixed sdk css line ending conflict (I think) and added queries to display the location on file for the current logged in user

--- ProviderAppver3/Controllers/HomeController.cs
+++ ProviderAppver3/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using GoogleMaps.LocationServices;
+using Microsoft.AspNet.Identity;
 using System;
+using System.Data;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +12,7 @@ namespace ProviderAppver3.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
@@ -27,10 +31,40 @@ namespace ProviderAppver3.Controllers
 
             return View();
         }
+        private ProviderDBV2Entities db = new ProviderDBV2Entities();
 
         public ActionResult Maps()
         {
-            var address = "1234 Bryce Ave, Aurora, Ohio";
+            string num;
+            string street;
+            string city;
+            string state;
+            string name;
+            int userid;
+            string id = User.Identity.GetUserId(); //saves currently logged in user id to string id
+            bool? provider = (from pr in db.AspNetUsers where pr.Id == id select pr.IsProvider).First(); //finds currently logged in user and checks if they are a provider or not then...
+            if ((bool)provider)
+            {
+                userid = (from p in db.Providers where p.UserName == id select p.ProviderID).First();
+                name = (from p in db.Providers where p.UserName == id select p.ProviderName).First();
+                num = (from a in db.Addresses where a.ProviderID == userid select a.StreetNumber).First();
+                street = (from a in db.Addresses where a.ProviderID == userid select a.StreetName).First();
+                city = (from a in db.Addresses where a.ProviderID == userid select a.City).First(); ;
+                state = (from a in db.Addresses where a.ProviderID == userid select a.State).First();
+            }
+            else
+            {
+                userid = (from c in db.Customers where c.UserName == id select c.CustomerID).First();
+                name = (from c in db.Customers where c.UserName == id select c.CustomerName).First();
+                num = (from a in db.Addresses where a.CustomerID == userid select a.StreetNumber).First();
+                street = (from a in db.Addresses where a.CustomerID == userid select a.StreetName).First();
+                city = (from a in db.Addresses where a.CustomerID == userid select a.City).First(); ;
+                state = (from a in db.Addresses where a.CustomerID == userid select a.State).First();
+
+            }
+
+
+            var address = num + " " + street + ", " + city + ", " + state;
             var locationService = new GoogleLocationService();
             var point = locationService.GetLatLongFromAddress(address);
 
@@ -38,6 +72,7 @@ namespace ProviderAppver3.Controllers
             var longitude = point.Longitude;
             ViewBag.Latitude = latitude;
             ViewBag.Longitude = longitude;
+            ViewBag.Name = name;
             return View();
 
         }
